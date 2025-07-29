18f5cf2c5c1628b0c92cbba933929f094d0f5b0f
DharmBharodiya <dharmbharodiya@gmail.com>
2025-07-29 22:56:34 +0530

    Started with new project. Notes App.

---
+++ notes-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div
+    className='max-w-lg mx-auto mt-10 p-6 bg-gray-200 rounded-lg shadow-lg'
+    >
+      <div
+      className='text-2xl font-bold text-center mb-4'
+      >📝Jot the Note</div>
+    </div>
+  )
+}
+
+export default App
