42df2488237b850aa1c36b23b074563cc31602bc
DharmBharodiya <dharmbharodiya@gmail.com>
2025-08-02 01:42:15 +0530

    ADD: Added a functionality such that the app could be operated with the keyboard with tabs and enter button.

--- TimerApp/src/components/TimerControl.jsx
+++ TimerApp/src/components/TimerControl.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { useEffect, useRef } from "react"
 
 function TimerControl({toggleTimer, isRunning, resetTimer, time}) {
+
+  const startButtonRef = useRef()
+
+  useEffect(() => {
+    if(startButtonRef.current){
+      startButtonRef.current.focus();
+    }
+  }, [])
+
+
   return (
     <div>
 
         <button
         className="text-white font-semibold px-4 py-1 bg-green-400 rounded-md mt-4 hover:bg-green-500 transition-all duration-100 cursor-pointer mr-4"
         onClick={toggleTimer}
+        ref={startButtonRef}
         >
           {isRunning ? "Pause" : (time > 0) ? "Continue" : "Start"}
         </button>
